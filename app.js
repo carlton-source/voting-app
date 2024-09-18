65e3586b973edf48e00041e7fb74ddbd2ecdd8e5
carlton-source <carlton.blessing.paul@gmail.com>
2024-09-18 02:21:17 +0100

    Set up authentication routes

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
-const app = express();
-
+const authRoutes = require('./routes/authRoutes');
 
 dotenv.config();
 
+const app = express();
 app.use(bodyParser.json());
+app.use('/auth', authRoutes);
+
 module.exports = app;
