6e8af4e43cc36ac5f27af318493c4d3a2fae73d6
carlton-source <carlton.blessing.paul@gmail.com>
2024-09-18 02:20:24 +0100

    Configure environment variables with dotenv

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
 const app = express();
+
+
+dotenv.config();
 
 app.use(bodyParser.json());
 module.exports = app;
