249858f133227ff651b4149ece5c52f64296051c
carlton-source <carlton.blessing.paul@gmail.com>
2024-09-18 16:37:27 +0100

    Refactor stacksService.js to add submitVote function for voting integration

--- voting-app/src/services/stacksService.js
+++ voting-app/src/services/stacksService.js
@@ -13,3 +13,31 @@ const { privateKey } = process.env;
 const { StacksTestnet } = require('@stacks/network');
 
 const network = new StacksTestnet();
+
+const submitVote = async (voterPrivateKey, candidateName) => {
+	const contractAddress = process.env.CONTRACT_ADDRESS;
+	const contractName = process.env.CONTRACT_NAME;
+	const functionName = 'submit-vote';
+
+	const candidateNameBuff = bufferCV(Buffer.from(candidateName));
+
+	const txOptions = {
+		contractAddress,
+		contractName,
+		functionName,
+		functionArgs: [candidateNameBuff],
+		senderKey: voterPrivateKey,
+		network,
+		postConditionMode: PostConditionMode.Allow,
+		anchorMode: AnchorMode.Any,
+	};
+
+	try {
+		const transaction = await makeContractCall(txOptions);
+		const broadcastResponse = await broadcastTransaction(transaction, network);
+		return broadcastResponse;
+	} catch (error) {
+		console.error('Error submitting vote:', error);
+		throw error;
+	}
+};
